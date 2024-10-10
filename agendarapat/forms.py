0ec92c5d1c3b14d6f56044f9ca86743b8decb29e
Arief Rachman <arief.rachman@ar-raniry.ac.id>
2024-10-10 15:37:38 +0700

    fix #7

--- agendarapat/forms.py
+++ agendarapat/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 from .models import AgendaRapat, NotulenRapat
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 class AgendaRapatForm(forms.ModelForm):
     class Meta:
         model = AgendaRapat
         fields = ['judul', 'tanggal', 'lokasi']
+        widgets = {
+            'tanggal' : DateInput(),
+        }
 
 class NotulenRapatForm(forms.ModelForm):
     class Meta:
